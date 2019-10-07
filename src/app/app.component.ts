7b3577a12d36e3913a088300ab37d3a414103929
Simon Schäffner <simon.schaeffner@googlemail.com>
2019-10-07 22:01:17 +0200

    improve table styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fabX Dashboard';
+  constructor(private meta: Meta) {
+    this.meta.addTag({ name: 'viewport', content: 'width=device-width, initial-scale=1.0' });
+  }
 }
