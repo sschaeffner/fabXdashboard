92e84e40c403aef05606b4fc26bb1bc793afa4e8
Simon Schäffner <simon.schaeffner@googlemail.com>
2019-11-07 17:07:32 +0100

    add Qualification.colour

--- src/app/shared/models/qualification.model.ts
+++ src/app/shared/models/qualification.model.ts
@@ -4,6 +4,7 @@ export class Qualification implements Deserializable {
   public id: number;
   public name: string;
   public description: string;
+  public colour: string;
 
   deserialize(input: any): this {
     return Object.assign(this, input);
